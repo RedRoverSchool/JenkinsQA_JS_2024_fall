625809953372ba4842b0915cfd61bb3115e05162
wwwser11 <wwwser11@yandex.ru>
2024-11-13 15:35:24 -0800

    add US_04.002 1st test

---
+++ cypress/e2e/US_04.002.cy.js
@@ -0,0 +1,60 @@
+/// <reference types="cypress" />
+
+
+
+describe('Folder Movement Tests', () => {
+
+    it('creates two folders', () => {
+        cy.get('span').contains('New Item').click()
+        cy.wait(1000)
+        cy.get('input[name="name"]').clear()
+        cy.get('input[name="name"]').type('Folder_01')
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
+        cy.get('[src="/static/c8d61ae5/images/title.svg"]').click()
+        cy.get('span').contains('New Item').click()
+        cy.wait(1000)
+        cy.get('input[name="name"]').clear()
+        cy.get('input[name="name"]').type('Folder_02')
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
+    });
+
+
+    it('Selects path to move a folder', () => {
+        cy.get('[href="job/Folder_02/"]').realHover({ position: 'center' })
+        cy.get('[data-href="http://localhost:8080/job/Folder_02/"]').should('be.visible')
+        cy.get('[data-href="http://localhost:8080/job/Folder_02/"]').click();
+        cy.get('[href="/job/Folder_02/move"]').should('be.visible')
+        cy.get('[href="/job/Folder_02/move"]').click();
+        cy.get('[name="destination"].select').should('be.visible')
+
+        cy.get('[name="destination"].select')
+            .find('option')
+            .contains('Folder_01')
+            .then(option => {
+                cy.get('[name="destination"].select').select(option.val());
+            });
+        cy.wait(3000)
+        cy.get('select[name="destination"]')
+            .invoke('val')               
+            .then(value => {
+                console.log(value);       
+                cy.log('Selected value:', value); 
+                expect(value).to.include('Folder_01')
+            });
+        cy.get('[name="Submit"]').click();
+    });
+
+    it('Delete creates two folders', () => {
+        cy.get('span').contains('Folder_01').scrollIntoView()
+        cy.get('span').contains('Folder_01').realHover()
+        cy.get('button[data-href="http://localhost:8080/job/Folder_01/"]').click()
+        cy.get('button[href="/job/Folder_01/doDelete"]').click()
+        cy.get('button.jenkins-button.jenkins-button--primary ').click()
+        cy.get('span').contains('Folder_01').should('not.exist')
+
+    });
+})
