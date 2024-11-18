77bd0794dc63a2501ea6161328976f09971900ae
Ion Negrescu <Negrescu29@users.noreply.github.com>
2024-11-18 21:41:41 +0100

    added test create new item from side bar

--- cypress/e2e/newItemCreateNewItem.cy.js
+++ cypress/e2e/newItemCreateNewItem.cy.js
@@ -1,6 +1,10 @@
 /// <reference types="cypress" />
 describe('US_00.000 | New Item > Create New item', () => {
     const jobName = 'Item_1'
+    const sidebarJobName = 'Item_2'
+    const LOCAL_PORT = Cypress.env('local.port');
+    const LOCAL_HOST = Cypress.env('local.host');
+
     it('TC_00.000-02 | New Item > Create New item | Create new item from "Create a job" button', () => {
         cy.get('span').contains('jobName').should('not.exist')
         cy.get('a[href="newJob"]').contains('Create a job').click()
@@ -9,5 +13,18 @@ describe('US_00.000 | New Item > Create New item', () => {
         cy.get('#ok-button').click()
         cy.get('a#jenkins-home-link').click()
         cy.get('table.jenkins-table.sortable').contains(jobName).should('exist')
+    })
+
+    it('TC_00.000-04 | New Item > Create New item | New item from left Sidebar', () => {
+        cy.get('span').contains('New Item').click()
+        cy.get('input#name').type(sidebarJobName)
+        cy.get('span.label').contains('Freestyle project').click();
+        cy.get('button#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('.job-index-headline')
+            .should('contain.text', sidebarJobName)
+            .should('be.visible')
+        cy.url()
+            .should('eq', `http://${LOCAL_HOST}:${LOCAL_PORT}/job/${sidebarJobName}/`);
     })
 })
