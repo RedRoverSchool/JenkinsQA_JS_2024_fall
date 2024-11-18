25a3d312826c0d11a20486ec6734a133ee4a8829
ivanlashkevich <lashkevich.lia@gmail.com>
2024-11-18 21:21:54 +0300

    remove credentials from the code

--- cypress/e2e/newItemCreateNewItem.cy.js
+++ cypress/e2e/newItemCreateNewItem.cy.js
@@ -2,11 +2,7 @@
 describe('US_00.000 | New Item > Create New item', () => {
     const jobName = 'Item_1'
     it('TC_00.000-02 | New Item > Create New item | Create new item from "Create a job" button', () => {
-        cy.visit('http://localhost:8080/login?from=%2F')
-        cy.get('input#j_username').type('admin')
-        cy.get('input#j_password').type('admin')
-        cy.get('button.jenkins-button.jenkins-button--primary').contains('Sign in').click()
-        cy.get('h1').contains('Welcome to Jenkins!').should('be.visible')
+        cy.get('span').contains('jobName').should('not.exist')
         cy.get('a[href="newJob"]').contains('Create a job').click()
         cy.get('input#name.jenkins-input').type(jobName)
         cy.get('.desc').eq(0).click()
