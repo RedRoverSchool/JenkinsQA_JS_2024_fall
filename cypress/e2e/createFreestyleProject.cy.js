21dc1a4417733b8cef709c3907f1075e2fc56bc5
Artem Diuzhikov <115650565+adiuzhikov@users.noreply.github.com>
2024-11-18 15:30:31 -0500

    TC_00.001.04 | New item > Create Freestyle Project > Verify a friendly reminder appeared when attempting to create a new Freestyle Project without a name (#99)
    * add_verify_reminder_test
    
    * add_empty_line

--- cypress/e2e/createFreestyleProject.cy.js
+++ cypress/e2e/createFreestyleProject.cy.js
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
   
 describe('US_00.001 | New item > Create Freestyle Project', () => {
-  it('Create a new Freestyle Project using the "New Item" button from the Dashboard', () => {
+    
+    it('Create a new Freestyle Project using the "New Item" button from the Dashboard', () => {
         cy.get(':nth-child(1) > .task-link-wrapper > .task-link').first().click();
         cy.get('.jenkins-input').type('Sandra');
         cy.get('.label').first().click();
@@ -10,8 +11,8 @@ describe('US_00.001 | New item > Create Freestyle Project', () => {
         cy.get('h1').should('have.text', 'Sandra');
         cy.get('.model-link').should('contain', 'Sandra');
     });
-  
-  it('TC_00.001.02 | New item > Create Freestyle Project > Verify a new freestyle project can be created from the Dahsboard page', () => {
+    
+    it('TC_00.001.02 | New item > Create Freestyle Project > Verify a new freestyle project can be created from the Dahsboard page', () => {
         
         const projectName = 'New Freestyle Project';
 
@@ -23,6 +24,17 @@ describe('US_00.001 | New item > Create Freestyle Project', () => {
 
         cy.get('#main-panel h1').should('have.text', projectName);
     
+    });
+
+    it('TC_00.001.04 | New item > Create Freestyle Project > Verify a friendly reminder appeared when attempting to create a new Freestyle Project without a name', () => {
+        
+        const emptyFieldReminder = '» This field cannot be empty, please enter a valid name'
+
+        cy.get('a[href$="/newJob"]').click();
+        cy.get('#items li[class$="FreeStyleProject"]').click();
+        
+        cy.get('div[class$="validation-message"]').should('have.text', emptyFieldReminder);
+
     });
     
 });
