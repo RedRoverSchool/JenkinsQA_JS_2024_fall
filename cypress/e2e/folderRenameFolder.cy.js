6ad1c5b04e0838100ae2931ff8129ddbc7d76de5
Natalia <natysechka.markova@gmail.com>
2024-11-18 23:38:05 -0500

    add another assertion

--- cypress/e2e/folderRenameFolder.cy.js
+++ cypress/e2e/folderRenameFolder.cy.js
@@ -4,8 +4,6 @@ describe('US_04.001 |Folder > Rename Folder', () => {
     it('Verify that error message is displayed when an invalid folder name is entered in the Rename Folder field', () => {
         cy.get('span').contains('New Item').click()
         cy.get('input[name="name"]').type('Folder*')
-        cy.get('div[id="itemname-invalid"]').invoke('text').then((text) => {
-            expect(text).to.include('is an unsafe character');
-        })
+        cy.get('div[id="itemname-invalid"]').should('contain', "is an unsafe character")
     });
-})
+});
