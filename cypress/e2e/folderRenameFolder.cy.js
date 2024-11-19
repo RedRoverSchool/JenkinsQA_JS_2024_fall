c201e01640c7cf685c4cfa53db7623885f34ab60
Natalia <natysechka.markova@gmail.com>
2024-11-18 23:11:17 -0500

    Add the right assertion

--- cypress/e2e/folderRenameFolder.cy.js
+++ cypress/e2e/folderRenameFolder.cy.js
@@ -4,7 +4,8 @@ describe('US_04.001 |Folder > Rename Folder', () => {
     it('Verify that error message is displayed when an invalid folder name is entered in the Rename Folder field', () => {
         cy.get('span').contains('New Item').click()
         cy.get('input[name="name"]').type('Folder*')
-        cy.get('div[id="itemname-invalid"]').contains('is an unsafe character')
-
+        cy.get('div[id="itemname-invalid"]').invoke('text').then((text) => {
+            expect(text).to.include('is an unsafe character');
+        })
     });
 })
