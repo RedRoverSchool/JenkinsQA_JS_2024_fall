0b96f9ea0d159076257059a4f5aa9f4702cda8f8
Jmadon <137956984+Jmadon@users.noreply.github.com>
2024-11-19 22:27:03 +0200

    Added search For NonExistent Item (#165)
    Co-authored-by: Jmadon <«Jmad.exe@gmail.com»>

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -15,4 +15,17 @@ describe("Header > Search Box", () => {
     cy.get("#search-box").type('{enter}');
     cy.get(".jenkins-app-bar h1").should('contain.text', 'testJob')
   });
+
+  it('TC_14.002-04 | Message that no matches found', ()=>{
+    let unicNameProject = "The most unique project name is 12-35658_312"
+    cy.get('input[name="q"]').type(unicNameProject + '{enter}')
+    cy.url().should('includes', '/search/')
+    // Check color and text contetn
+    cy.get('div#main-panel > h1').should('have.css', 'color', 'rgb(20, 20, 31)')
+    cy.get('div#main-panel > h1').contains("Search for 'The most unique project name is 12-35658_312'")
+    // Check color and text error massage
+    cy.get('div#main-panel > div.error').should('have.css', 'color', 'rgb(230, 0, 31)')
+    cy.get('div#main-panel > div.error').contains("Nothing seems to match.")
+  })
+  
 });
