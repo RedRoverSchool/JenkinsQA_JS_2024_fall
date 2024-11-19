f97cef3dc4ae808274b593cda275c8fe4678588c
Anton Kolupaiev <antnikol@gmail.com>
2024-11-19 20:31:46 +0200

    update test

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -2,10 +2,17 @@
 
 describe("Header > Search Box", () => {
   it("Header > Search Box | User can select suggestion to auto-fill and complete the search", () => {
+    cy.get('a[href="/view/all/newJob"]').click();
+    cy.get('.jenkins-input').type('testJob');
+    cy.get('.label').contains('Freestyle project').click();
+    cy.get('#ok-button').click();
+    cy.get('textarea[name="description"]').type('...some description...')
+    cy.get('button[formnovalidate="formNoValidate"]').click();
+    cy.get('a[href="/"]').first().click();
     cy.get("#search-box").click();
-    cy.get("#search-box").type("s");
+    cy.get("#search-box").type("te");
     cy.get("#search-box-completion li").first().click();
     cy.get("#search-box").type('{enter}');
-    cy.get(".jenkins-app-bar h1").should('contain.text', 'SYSTEM')
+    cy.get(".jenkins-app-bar h1").should('contain.text', 'testJob')
   });
 });
