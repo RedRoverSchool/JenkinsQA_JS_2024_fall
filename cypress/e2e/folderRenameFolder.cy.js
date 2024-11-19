ee22e647d7654862930525fa4e194c7a12f3608f
Natalia <natysechka.markova@gmail.com>
2024-11-18 22:48:58 -0500

    Remove unused variables and steps

--- cypress/e2e/folderRenameFolder.cy.js
+++ cypress/e2e/folderRenameFolder.cy.js
@@ -1,26 +1,10 @@
 /// <reference types="cypress"/>
-const LOCAL_PORT = Cypress.env('local.port');
-const LOCAL_HOST = Cypress.env('local.host');
 
 describe('US_04.001 |Folder > Rename Folder', () => {
-        it('Verify that error message is displayed when an invalid folder name is entered in the Rename Folder field', () => {
-            cy.get('span').contains('New Item').click()
-            cy.wait(1000)
-            cy.get('input[name="name"]').clear()
-            cy.get('input[name="name"]').type('Folder1')
-            cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
-            cy.get('button').contains('OK').click()
-            cy.get('button').contains('Save').click()
-            cy.get('#jenkins-home-link').click()
-            cy.get('[href="job/Folder1/"]').realHover({ position: 'center' })
-            cy.get(`[data-href="http://${LOCAL_HOST}:${LOCAL_PORT}/job/Folder1/"]`).should('be.visible')
-            cy.get(`[data-href="http://${LOCAL_HOST}:${LOCAL_PORT}/job/Folder1/"]`).click();
-            cy.get('a[href="/job/Folder1/confirm-rename"]').click()
-            cy.get('input[name="newName"]').clear()
-            cy.get('input[name="newName"]').type('Folder*')
-            cy.get('button[name="Submit"]').click()
-            cy.get('div[id="main-panel"]').contains('Error')
+    it('Verify that error message is displayed when an invalid folder name is entered in the Rename Folder field', () => {
+        cy.get('span').contains('New Item').click()
+        cy.get('input[name="name"]').type('Folder*')
+        cy.get('div[id="itemname-invalid"]').contains('is an unsafe character')
 
-       
-});
+    });
 })
