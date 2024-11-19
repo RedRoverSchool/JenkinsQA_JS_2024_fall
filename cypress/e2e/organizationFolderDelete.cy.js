0023edf945bb9b4e482fc7fdb7773773265cfcbb
Alzhan-B <alzhan.braliyev@gmail.com>
2024-11-19 18:45:21 -0500

    TC_06.005-03 Deleting Organization folder on Dashboard main page

--- cypress/e2e/organizationFolderDelete.cy.js
+++ cypress/e2e/organizationFolderDelete.cy.js
@@ -14,12 +14,23 @@ describe('US_06.005 | Organization folder > Delete Organization Folder', () => {
     it('TC_06.005-01 | Delete Organization Folder on project page', () => {
         cy.get('[class="task "]').contains("Delete Organization Folder").click();
         cy.get('button').contains('Yes').click();
+        
         cy.get('[id="main-panel"]').should('not.contain.text', name);
     });
 
     it('TC_06.005-02 | Delete Organization Folder via folder dropdown menu under the header', () => {
         cy.get(':nth-child(3) > .model-link').contains(name).trigger('mouseover');
         cy.get(':nth-child(3) > .model-link > .jenkins-menu-dropdown-chevron').click({force:true});
+        cy.get('.jenkins-dropdown > [href$="elete"]').click();
+        cy.get('button').contains('Yes').click();
+ 
+        cy.get('[id="main-panel"]').should('not.contain.text', name);
+    });
+
+    it('TC_06.005-03 | Delete Organization Folder from the list of folders on the Dashboard main page', () => {
+        cy.get('[id="jenkins-home-link"]').click();
+        cy.get('.jenkins-table__link').contains(name).trigger('mouseover');
+        cy.get('.jenkins-table__link > .jenkins-menu-dropdown-chevron').click({force:true});
         cy.get('.jenkins-dropdown > [href$="elete"]').click();
         cy.get('button').contains('Yes').click();
  
