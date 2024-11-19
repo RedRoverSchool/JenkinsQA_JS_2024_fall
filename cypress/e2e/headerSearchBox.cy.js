cc118e510f40b93eb688fc0d6529711f19e3e584
antnikol <antnikol@gmail.com>
2024-11-19 19:46:37 +0200

    .skip test in headerSearchBox.cy.js

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Header > Search Box", () => {
-  it("Header > Search Box | User can select suggestion to auto-fill and complete the search", () => {
+  it.skip("Header > Search Box | User can select suggestion to auto-fill and complete the search", () => {
     cy.get("#search-box").click();
     cy.get("#search-box").type("s");
     cy.get("#search-box-completion li").first().click();
