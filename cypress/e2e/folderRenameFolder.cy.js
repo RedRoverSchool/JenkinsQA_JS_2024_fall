3e53ea39b1f7926e21007dfecda10ef31807a406
wwwser11 <wwwser11@yandex.ru>
2024-11-18 22:00:37 -0800

    TC_04.001.08 | Folder > Rename Folder | Enter a valid folder name with numbers and allowed special characters.

--- cypress/e2e/folderRenameFolder.cy.js
+++ cypress/e2e/folderRenameFolder.cy.js
@@ -4,16 +4,16 @@ const PASSWORD = Cypress.env('local.admin.password');
 const LOCAL_PORT = Cypress.env('local.port');
 const LOCAL_HOST = Cypress.env('local.host');
 
-describe('Folder > Rename Folder', () => {
 
-    it('Successfully enter a valid folder name in the special field', () => {
+describe('US_04.001 | Folder > Rename Folder', () => {
+    it('TC_04.001.06 | Folder > Rename Folder | Successfully enter a valid folder name in the special field', () => {
         //preconditions
-        cy.get('span').contains('New Item').click();
-        cy.get('input[name="name"]').clear();
-        cy.get('input[name="name"]').type('Test Folder');
-        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click();
-        cy.get('button').contains('OK').click();
-        cy.get('button').contains('Save').click();
+        cy.get('span').contains('New Item').click()
+        cy.get('input[name="name"]').clear()
+        cy.get('input[name="name"]').type('Test Folder')
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
         cy.get('div[id="breadcrumbBar"]').contains('Test Folder').should('exist');
         cy.visit(`${LOCAL_HOST}:${LOCAL_PORT}`);
         //steps
@@ -22,9 +22,9 @@ describe('Folder > Rename Folder', () => {
         cy.get('.jenkins-dropdown__item[href="/job/Test%20Folder/confirm-rename"]').click();
         cy.get('[checkdependson="newName"]').click();
         cy.get('[checkdependson="newName"]').type(' 1219').should('have.value', 'Test Folder 1219'); 
-    });
+    })
 
-    it('Enter a folder name in a non-Latin language', () => {
+    it('TC_04.001.07 | Folder > Rename Folder Enter a folder name in a non-Latin language', () => {
         //preconditions
         cy.get('span').contains('New Item').click();
         cy.get('input[name="name"]').clear();
@@ -51,7 +51,7 @@ describe('Folder > Rename Folder', () => {
         cy.get('[data-id="ok"]').click();
     });
 
-    it('Enter a valid folder name with numbers and allowed special characters', () => {
+    it('TC_04.001.08 | Folder > Rename Folder | Enter a valid folder name with numbers and allowed special characters', () => {
         //preconditions
         cy.get('span').contains('New Item').click();
         cy.get('input[name="name"]').clear();
