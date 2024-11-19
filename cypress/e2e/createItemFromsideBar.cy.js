4c9702e81aec06e8e15dad437a1da4b87e10dd64
Yuliya <yulka818818@gmail.com>
2024-11-18 21:51:08 -0300

    add changes to descride

--- cypress/e2e/createItemFromsideBar.cy.js
+++ cypress/e2e/createItemFromsideBar.cy.js
@@ -1,15 +1,19 @@
 /// <reference types="cypress" />
 
-describe('create a "New item', () => {
+describe('New Item > Create New item', () => {
 
-it('create new item from the "New Item" link in the left sidebar', () => {
-    cy.get(':nth-child(1) > .task-link-wrapper > .task-link').click()
-    cy.url().should('include', '/newJob')
-    cy.get('input[name="name"]').type("test2")
-    cy.get(".hudson_model_FreeStyleProject").click()
-    cy.get("#ok-button").click()
-    cy.get('.jenkins-submit-button').click()
-    cy.url().should('include', '/test2')
-    cy.get("#main-panel").should("contain", "test2").and('exist')
-});
-});
+    it('create new item from the "New Item" link in the left sidebar', () => {
+      cy.get(':nth-child(1) > .task-link-wrapper > .task-link').click();
+      cy.url().should('include', '/newJob');
+  
+      cy.get('input[name="name"]').type("test2");
+      cy.get(".hudson_model_FreeStyleProject").click();
+      cy.get("#ok-button").should('be.visible').click(); // Ensure the button is visible
+      cy.get('.jenkins-submit-button').click();
+  
+      cy.url().should('include', '/test2');
+      cy.get("#main-panel").should("contain", "test2").and('exist');
+    });
+  
+  });
+  
