624336b67d77b172cb5831fef4a559a7beaa6296
wwwser11 <wwwser11@yandex.ru>
2024-11-18 17:13:56 -0800

    add Rename Folder | Enter a valid folder name with numbers and allowed special characters (e.g., underscores)

--- cypress/e2e/renameFolder.cy.js
+++ cypress/e2e/renameFolder.cy.js
@@ -22,4 +22,28 @@ describe('Folder > Rename Folder', () => {
         cy.get('[checkdependson="newName"]').click();
         cy.get('[checkdependson="newName"]').type(' 1219').should('have.value', 'Test Folder 1219'); 
     })
+
+    it('Enter a valid folder name with numbers and allowed special characters', () => {
+        //preconditions
+        cy.get('span').contains('New Item').click();
+        cy.get('input[name="name"]').clear();
+        cy.get('input[name="name"]').type('Test Folder');
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click();
+        cy.get('button').contains('OK').click();
+        cy.get('button').contains('Save').click();
+        cy.get('div[id="breadcrumbBar"]').contains('Test Folder').should('exist');
+        cy.visit(`${LOCAL_HOST}:${LOCAL_PORT}`);
+        //steps
+        cy.get('[href="job/Test%20Folder/"]').realHover();
+        cy.get('[href="job/Test%20Folder/"] .jenkins-menu-dropdown-chevron').click();
+        cy.get('.jenkins-dropdown__item[href="/job/Test%20Folder/confirm-rename"]').click();
+        cy.get('[checkdependson="newName"]').click().clear();
+        cy.get('[checkdependson="newName"]').type('Test_Folder7+').should('have.value', 'Test_Folder7+');
+        cy.get('[formnovalidate="formNoValidate"]').click();
+        cy.get('h1').invoke('text')
+        .then((text) =>{
+            expect(text.trim()).to.eq('Test_Folder7+');
+        })
+    });
+
 });
