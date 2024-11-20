ec45b0a86158342e8326fc292523eda82326960a
Juli Diva <diva.julii@gmail.com>
2024-11-19 21:05:16 -0500

    add_delete_project_from_dashboard

--- cypress/e2e/freestyleProjectDeleteProject.cy.js
+++ cypress/e2e/freestyleProjectDeleteProject.cy.js
@@ -15,4 +15,21 @@ describe('US_01.004 | FreestyleProject > Delete Project', ()=>{
         cy.get('button.jenkins-button.jenkins-button--primary ').click();
         cy.get('#main-panel h1').should('have.text', "Welcome to Jenkins!");
     })
+    it('TC_00.004.01 | Verify user able to delete existing project from Dashboard', () =>{
+
+        cy.get('span.task-link-text').contains("New Item").click({force:true});
+        cy.get('input[name="name"]').type('Pro1');
+        cy.get('span.label').contains('Freestyle project').click();
+        cy.get('button').contains("OK").click();
+        cy.get('button').contains("Save").click();
+        cy.get('a').contains("Dashboard").click();
+        cy.get('td>a>button[class="jenkins-menu-dropdown-chevron"]').click({force:true});
+        cy.get('div[class="jenkins-dropdown"]')
+        cy.get('div>button[href="/job/Pro1/doDelete"]').click({force:true});
+        cy.get("button[data-id='ok']").contains("Yes").click();
+        
+        cy.get('#main-panel h1').should('have.text', "Welcome to Jenkins!");
+
+    })
+
 })
