aa2f19ab80e53208c3b7df47b5587d903dbc6534
Natalia Markova <167388746+Ridgewood2016@users.noreply.github.com>
2024-11-20 18:15:34 -0500

    TC_01.006.02 | FreestyleProject > Move project| Choose from a list of available folders (#232)
    * Added TC_01.006.02 | FreestyleProject > Move project| Choose from a list of available folders
    
    * Added correct assertion

--- cypress/e2e/freestyleProjectMoveProject.cy.js
+++ cypress/e2e/freestyleProjectMoveProject.cy.js
@@ -25,6 +25,38 @@ describe ('US_01.006 | FreestyleProject > Move project', () => {
                 .and('include','NewFolder')
  
     })
+
+    const LOCAL_PORT = Cypress.env('local.port');
+    const LOCAL_HOST = Cypress.env('local.host');
+
+    it('TC_01.006.02 |Choose from a list of available folders',() => {
+        cy.get('span').contains('New Item').click()
+        cy.get('#name').type('Project')
+        cy.get('span.label').contains('Freestyle project').click()
+        cy.get('#ok-button').click({force: true})
+        cy.get('[name="Submit"]').click()
+        cy.url().should('include', 'Project')
+        cy.get('#jenkins-home-link').click()
+        cy.get('span').contains('New Item').click()
+        cy.get('input[name="name"]').type('Folder1')
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
+        cy.get('#jenkins-home-link').click()
+        cy.get('span').contains('New Item').click()
+        cy.get('input[name="name"]').type('Folder2')
+        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
+        cy.get('#jenkins-home-link').click()
+        cy.get('[href="job/Project/"]').realHover({ position: 'center' })
+        cy.get(`button[data-href="http://${LOCAL_HOST}:${LOCAL_PORT}/job/Project/"]`).click()
+        cy.get('a[href="/job/Project/move"]').click()
+        cy.get('[name="destination"]').select('/Folder2')
+        cy.get('button').contains('Move').click()
+
+        cy.get('div[id="main-panel"]').should('contain','Folder2/Project')
+    })
  
 })
  
