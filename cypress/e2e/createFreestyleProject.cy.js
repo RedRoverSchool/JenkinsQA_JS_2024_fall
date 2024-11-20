29ce0c1dbc4ae9bf4740e8b01b87ad5dc0f123cd
LubovLepekhina <lubov.lepekhina@gmail.com>
2024-11-20 16:34:15 +0100

    add TC_00.001.12

--- cypress/e2e/createFreestyleProject.cy.js
+++ cypress/e2e/createFreestyleProject.cy.js
@@ -147,4 +147,14 @@ describe('US_00.001 | New item > Create Freestyle Project', () => {
         cy.get('[id="main-panel"]').should('contain.text', projectName).and('be.visible');
     });
 
+    it.only('TC_00.001.12 | Verify that space projects name is not accepted during project creation', () => {
+        cy.get('a:contains("New Item")').click();
+        cy.get('input#name').type(' ');
+        cy.get('div').contains('Freestyle project').click();
+        cy.get('button#ok-button').click();
+
+        cy.get('#main-panel').should('include.text', 'Error')
+                             .should('include.text', 'No name is specified');
+    });
+
 });
