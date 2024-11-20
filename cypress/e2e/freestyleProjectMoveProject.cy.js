0d1e1acbe4d812cb6b2d39956174395a2ab5b674
hobbita <hobbita@yandex.ru>
2024-11-21 00:20:26 +0300

    added TC_01.006.04

--- cypress/e2e/freestyleProjectMoveProject.cy.js
+++ cypress/e2e/freestyleProjectMoveProject.cy.js
@@ -25,6 +25,32 @@ describe ('US_01.006 | FreestyleProject > Move project', () => {
                 .and('include','NewFolder')
  
     })
+    it('TC_01.006.04 | FreestyleProject > Move project | from Dashboard', () => {
+        cy.get('span.task-link-text').eq(0).click({force:true})
+        cy.get('input[id="name"]').click().type('New Folder')
+        cy.get('li.com_cloudbees_hudson_plugins_folder_Folder').click({force: true})
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
+        cy.visit('http://localhost:8080/')
+        cy.get('span.task-link-text').eq(0).click({force:true})
+        cy.get('input[id="name"]').click().type('New Job')
+        cy.get('li.hudson_model_FreeStyleProject').click({force: true})
+        cy.get('button').contains('OK').click()
+        cy.get('button').contains('Save').click()
+
+        cy.visit('http://localhost:8080/')
+        cy.get('button.jenkins-menu-dropdown-chevron').eq(3).click({force: true})
+        cy.get('a.jenkins-dropdown__item').contains('Move').invoke('text').then((Text) => {
+            const trimtxt=Text.trim()
+            expect(trimtxt).to.equal('Move')
+
+        }).wait(1000)
+        cy.get('a.jenkins-dropdown__item').contains('Move').click()
+        cy.get('select.setting-input').select(1)
+        cy.get('button').contains('Move').click()
+        cy.visit('http://localhost:8080/job/New%20Folder/').contains('New Job')
+
+    })
  
 })
  
