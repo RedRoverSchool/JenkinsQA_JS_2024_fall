8e39925a354f8bb8f12ec43c1c4114113814cba2
Belkacole <colesnicova777@gmail.com>
2024-11-20 10:20:37 -0500

    add_changes

--- cypress/e2e/headerClickLogOut.cy.js
+++ cypress/e2e/headerClickLogOut.cy.js
@@ -7,4 +7,9 @@ describe('US_14.003| Header > Click Log out', () => {
         cy.url().should('eq', 'http://localhost:8080/login?from=%2F')
     })
 
+    it('TC_14.003.02-A | Verify Log out button is seen and works properly.', () => {
+        cy.get('a[href="/logout"]').click();
+
+        cy.get('#main-panel h1').should('be.visible').and('have.text', 'Sign in to Jenkins');
+    });
 })
