423fb609b8e5577d0d879a3413257e70510514ce
Infared! <ekkirkitadze@gmail.com>
2024-11-20 19:15:47 +0500

    fix assert

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -8,6 +8,8 @@ describe('US_14.002 | Header > Search Box', () => {
     it('TC_14.002.01 | Auto-Completion Suggestion Selection', () => {
         cy.get('input[id="search-box"]').click().clear()
         .type('man')
-        cy.get('li').contains('manage').click().should('be.visible')
+        cy.get('li').contains('manage')
+        .click()
+        cy.get('li').contains('manage').should('be.visible')
     })
 })
