36cc8045ddf442d59f4e353239e43615e93b46da
Natalia <natysechka.markova@gmail.com>
2024-11-20 08:50:05 -0500

    Added TC_04.001.03-A

--- cypress/e2e/renameFolder.cy.js
+++ cypress/e2e/renameFolder.cy.js
@@ -51,21 +51,6 @@ describe('Folder > Rename Folder', () => {
         cy.get('[data-id="ok"]').click();
     });
 
-    it('Verify that error message is displayed when an invalid folder name is entered in the Rename Folder field', () => {
-        cy.get('span').contains('New Item').click()
-        cy.get('input[name="name"]').type('Folder1')
-        cy.get('.com_cloudbees_hudson_plugins_folder_Folder').click()
-        cy.get('button').contains('OK').click()
-        cy.get('button').contains('Save').click()
-        cy.get('#jenkins-home-link').click()
-        cy.get('[href="job/Folder1/"]').realHover({ position: 'center' })
-        cy.get(`[data-href="http://${LOCAL_HOST}:${LOCAL_PORT}/job/Folder1/"]`).should('be.visible')
-        cy.get(`[data-href="http://${LOCAL_HOST}:${LOCAL_PORT}/job/Folder1/"]`).click();
-        cy.get('a[href="/job/Folder1/confirm-rename"]').click()
-        cy.get('input[name="newName"]').clear()
-        cy.get('input[name="newName"]').type('Folder*')
-        cy.get('button[name="Submit"]').click()
-        cy.get('div[id="main-panel"]').should('contain', 'is an unsafe character')
-    });
+    
 })
 
