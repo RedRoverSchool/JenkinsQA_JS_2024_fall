f376fd82a48434e25259fa0e24f3143289fcfa26
Iris20221 <irina.velichcko2014@gmail.com>
2024-11-21 15:50:55 -0600

    Save local changes before updating

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -122,6 +122,13 @@ describe('US_14.002 | Header > Search Box', () => {
     })
   })
 
+  it.only('TC_14.002.14 | Upon selecting a suggestion, the user is redirected to the relevant destination page', () => {
+    cy.get('#search-box').type('con')
+    cy.contains('configure').click()
+    cy.get('body').type('{enter}')
+
+  })
+
   it('TC_14.002.12 | Verify that Dashboard page in Jenkins has a search box on its top right', () => {
     cy.get('.main-search__input').should('be.visible').and('have.attr', 'placeholder');
   })
