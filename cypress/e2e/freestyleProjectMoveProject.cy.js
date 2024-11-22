49fd39407400a2ea74ec5eb426443722c9128390
Olga Tsoy <olgakim.us19@gmail.com>
2024-11-22 15:08:37 -0600

    addTC_01.006.03_verifyProjectMovedFromDashboard

--- cypress/e2e/freestyleProjectMoveProject.cy.js
+++ cypress/e2e/freestyleProjectMoveProject.cy.js
@@ -1,5 +1,38 @@
-
 ///<reference types = 'cypress'/>
+import { faker } from "@faker-js/faker"
+
+const btnNewItem = 'a[href="/view/all/newJob"]'
+const inputEnterAnItemName = '#name'
+const btnOK = '#ok-button'
+const btnSave = 'button[name="Submit"]'
+const btnMove = 'button[name="Submit"]'
+const breadcrumbDashboard = '#breadcrumbs > li:nth-child(1) > a'
+const btnItemDropdownMove = 'a[href*="move"]'
+const selectDropdown = '.select'
+const randomFolderName = faker.commerce.productName()
+const randomProjectName = faker.commerce.productName()
+
+/**
+ * Method to create a new item : 
+ * createNewItem(itemName, itemType) 
+ * @param {*give any new item name} itemName 
+ * @param {*choose item type option} itemType
+ * @options of item type 
+ * 'Freestyle project', 'Pipeline', 'Multi-configuration project', 'Folder', 'Multibranch Pipeline','Organization Folder'
+ */
+const createNewItem = (itemName, itemType) => {
+    cy.get(breadcrumbDashboard).click()
+    cy.get(btnNewItem).click()
+    cy.get(inputEnterAnItemName).type(itemName)
+    cy.get('label span').contains(itemType).click()
+    cy.get(`.j-item-options li[class="hudson_model_${itemType}"]`).click()
+    cy.get(btnOK).click({force: true})
+    cy.get(btnSave).click()
+}
+
+const getExistedItem = (name) => {
+    return cy.get(`a[href*="job/${name}"]`)
+}
 
 describe ('US_01.006 | FreestyleProject > Move project', () => {
 
@@ -58,5 +91,23 @@ describe ('US_01.006 | FreestyleProject > Move project', () => {
         cy.get('div[id="main-panel"]').should('contain','Folder2/Project')
     })
  
+    it('TC_01.006.03 | Verify a project is moved from the Dashboard page after clicking move',() => {
+        
+        cy.log('Precondition: create a folder and project')
+        createNewItem(randomFolderName, "Folder")
+        createNewItem(randomProjectName, "Freestyle project")
+        
+        cy.log('Steps')
+        cy.get(breadcrumbDashboard).click()
+        const expectedName = randomProjectName.replaceAll(" ", "%20")
+        cy.get(`a[href*="job/${expectedName}"]`).realHover()
+        cy.get(`a[href*="job/${expectedName}"] .jenkins-menu-dropdown-chevron`).click()
+        cy.get(btnItemDropdownMove).click()
+        cy.get(selectDropdown).select(`/${randomFolderName}`)
+        cy.get(btnMove).click()
+        cy.get(breadcrumbDashboard).click()
+
+        getExistedItem(randomProjectName).should('not.exist')
+    })
 })
  
