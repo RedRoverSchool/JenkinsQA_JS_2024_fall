7e6b4daffaa37f47d9d047e54f259c365b0cdca9
Tatiana BM <burykina.tatiana@gmail.com>
2024-11-22 23:46:48 +0100

    TC_14.002.16 | Header > Search Box | Finds a build by its number

--- cypress/e2e/headerSearchBox.cy.js
+++ cypress/e2e/headerSearchBox.cy.js
@@ -133,5 +133,36 @@ describe('US_14.002 | Header > Search Box', () => {
     cy.get('header').should("exist")
     cy.get('#search-box').should("exist")
   })
+
+  it('TC_14.002.16 | Finds a build by its number', () => {
+    //create first build
+    cy.contains('.task-link', 'New Item').click()
+    cy.get('input#name').type(newJobFolderName)
+    cy.get('li[class*="FreeStyleProject"]').click()
+    cy.get('button#ok-button').click()
+    cy.get('button[name="Submit"]').click()
+    cy.contains(`a[href*="/job/${newJobFolderName}/build"]`, 'Build Now').click()
+    cy.get('#breadcrumbs li:first-child').click()
+    //create second build
+    cy.get(`td:last-child a[href*="job/${newJobFolderName}/build"]`).click()
+    //cy.get('input#search-box').type(newJobFolderName + ' ' + searchBuildNumber)
+    cy.get(`td:nth-child(3) a[href="job/${newJobFolderName}/"]`).click()
+    // get all builds
+    cy.get('a.build-link.display-name').then(($els)=>{
+      return Cypress.$.makeArray($els).map($el => $el.innerText)
+    }).as('arrayAllBuilds')
+    //search by build number
+    cy.get('@arrayAllBuilds').then(($array) => {
+      $array.forEach(build => {
+        cy.get('input#search-box').type(`${newJobFolderName} ${build}`)
+        cy.get('li[style]:not([style="display: none;"])')
+            .should('be.visible')
+            .and('contain', `${newJobFolderName} ${build}`)
+        cy.get('input#search-box').type('{enter}')
+        cy.get('.jenkins-build-caption h1').should('contain', build) 
+        cy.url().should('contain', build.slice(1))
+      })
+    });
+  })
 });
 
