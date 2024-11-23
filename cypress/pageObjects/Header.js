389a9efbe785a0221d32773a403d0d98078c4a18
age96 <36861426+age96@users.noreply.github.com>
2024-11-23 16:56:11 -0500

    RF_14.002.05 Search by auto-fill

---
+++ cypress/pageObjects/Header.js
@@ -0,0 +1,27 @@
+/// <reference types="cypress" />
+import JobPage from "./JobPage"
+class Header{
+
+    getSearchField = ()=> cy.get("#search-box")
+    getSearchOption = () =>  cy.get("#search-box-completion li")
+
+    typeSearchTerm(term)
+    {
+       this.getSearchField().type(term)
+       return this
+    }
+
+    clickSearchOption()
+    {
+        this.getSearchOption().first().click()
+        return this
+    }
+
+    searchTerm ()
+    {
+        this.getSearchField().type('{enter}')
+        return new JobPage()
+    }
+
+}
+export default Header
