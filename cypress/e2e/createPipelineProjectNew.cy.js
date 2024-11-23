175dd1cd506434ffeaa53357c20c6364ff00c4a6
Irina-Didorenko <aminalina2010@gmail.com>
2024-11-23 19:51:30 +0100

    Update createPipelineProjectNew.cy.js

--- cypress/e2e/createPipelineProjectNew.cy.js
+++ cypress/e2e/createPipelineProjectNew.cy.js
@@ -1,6 +1,6 @@
 describe('US_00.002 | New Item > Create Pipeline Project #14', () => {
     
-    it.only('TC_00.002.006 | New Pipeline Project Create', () => {
+    it('TC_00.002.006 | New Pipeline Project Create', () => {
         let itemName = 'MyTestPipeline'
         cy.get('span').contains('New Item').click()
         cy.get('#name').type(itemName)
