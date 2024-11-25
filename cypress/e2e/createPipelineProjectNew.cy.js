06cb1d36d9c0f06db05210de74f2f0e683f3e820
Irina-Didorenko <aminalina2010@gmail.com>
2024-11-25 09:25:36 +0100

    TC_00.002.006 | New Pipeline Project Create (#334)
    Co-authored-by: ididorenko <irina.didorenko@soxes.ch>

--- cypress/e2e/createPipelineProjectNew.cy.js
+++ cypress/e2e/createPipelineProjectNew.cy.js
@@ -10,4 +10,25 @@ describe('US_00.002 | New Item > Create Pipeline Project #14', () => {
         cy.get('button[name="Submit"]').click({ force: true })     
         cy.get('.model-link').click()
         cy.get(`#job_${itemName}`).should('exist')
-    })})
+    })
+
+    it('TC_00.002.007 | New Pipeline Project check Item name valid', () => {
+        let symbols = '!@#$%^&*\|:?><][;/'
+        cy.get('span').contains('New Item').click()
+        for (let i = 0; i < symbols.length; i++){
+            cy.get('#name').clear()
+            cy.get('#itemname-invalid').should('have.class', 'input-message-disabled')
+            cy.get('#name').type(symbols[i])
+            cy.get('#itemname-invalid')
+            .should('not.have.class', 'input-message-disabled')
+            .should('have.text', `» ‘${symbols[i]}’ is an unsafe character`)
+            .should('have.css', 'color', 'rgb(230, 0, 31)')
+        }
+        cy.get('#name').clear()  
+        cy.get('#name').type('.')
+        cy.get('#itemname-invalid')
+        .should('not.have.class', 'input-message-disabled')
+        .should('have.text', '» “.” is not an allowed name')
+        .should('have.css', 'color', 'rgb(230, 0, 31)')            
+    })
+})
