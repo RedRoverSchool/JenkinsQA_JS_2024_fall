a2dfecc362a2d3a510c496a9555beb3cc86bf2ff
antnikol <antnikol@gmail.com>
2024-11-25 21:30:57 +0200

    RF_00.002.01 | New Item > Create Pipeline Project | Special character… (#369)
    * RF_00.002.01 | New Item > Create Pipeline Project | Special characters are not allowed in the project name
    
    * update TC_00.002.01 | Special characters are not allowed in the project name"
    
    * upd2/ RF_00.002.01 | Special characters are not allowed in the project name
    
    * upd3/ TC_00.002.01 | Special characters are not allowed in the project name
    
    * restor okBtnClick()

--- cypress/pageObjects/DashboardPage.js
+++ cypress/pageObjects/DashboardPage.js
@@ -1,10 +1,14 @@
+/// <reference types="cypress" />
 import NewJobPage from "./NewJobPage"
 
-class DashboardPage
-{
+class DashboardPage {
+
     getNewItemLink = ()=>  cy.get('a[href="/view/all/newJob"]')
 
-
+    clickNewItemMenuLink() {
+        this.getNewItemLink().click({ force: true });
+        return new NewJobPage();
+    }
 
     addNewProj()
     {
