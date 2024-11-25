efd928e0a73f2f6b65d377a354654f980f98fa8c
Anton Kolupaiev <antnikol@gmail.com>
2024-11-25 23:34:06 +0200

    TC_14.003.03-A | Header > Log out option | The "Session ID" cookie is changed after Log out

--- cypress/pageObjects/DashboardPage.js
+++ cypress/pageObjects/DashboardPage.js
@@ -1,15 +1,28 @@
+import { th } from "@faker-js/faker"
 import NewJobPage from "./NewJobPage"
+import LoginPage from "./LoginPage"
 
 class DashboardPage
 {
     getNewItemLink = ()=>  cy.get('a[href="/view/all/newJob"]')
+    getLogOutButton = () => cy.get('a[href="/logout"]')
 
 
-
-    addNewProj()
-    {
+    addNewProj() {
         this.getNewItemLink().click()
         return new NewJobPage()
     }
+
+    clickLogOutButton() {
+        this.getLogOutButton().click()
+        return new LoginPage()
+    }
+
+    getSessionCookie(cookieName) {
+        return cy.getCookies().then((cookies) => {
+          return (cookies.find((cookie) => cookie.name.includes(cookieName))).value;
+        });
+    }
+
 }
 export default DashboardPage
