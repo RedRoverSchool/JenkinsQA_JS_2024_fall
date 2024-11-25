8ad5772355a065ce59e205afdfaa0214ee685203
Yuliya Bozheva <yulka818818@gmail.com>
2024-11-24 22:02:59 -0300

    RF_14.002.06 | Multiple matches are displayed on the result page  (#362)
    * add changes to the POM 14.002.006
    
    * add changes
    
    * add changes

--- cypress/pageObjects/SearchResultsPage.js
+++ cypress/pageObjects/SearchResultsPage.js
@@ -3,6 +3,8 @@
 class SearchResultsPage {
 
     getNoMatchesErrorMessage = () => cy.get('.error');
+    getConfigItem = () => cy.get('#item_config');
+    getConfigureItem = () => cy.get('#item_configure');
 
 }
 
