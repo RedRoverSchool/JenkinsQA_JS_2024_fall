7a455b5236117f61ee20bf5ef4784d6e3139626d
Yuliya Bozheva <yulka818818@gmail.com>
2024-11-25 22:15:19 -0300

    RF_14.02.008 | Case insensitive search (#386)
    * add changes POM 14.02.008
    
    * add changes 14.002.008

--- cypress/pageObjects/Header.js
+++ cypress/pageObjects/Header.js
@@ -6,6 +6,8 @@ class Header {
 
     getSearchField = () => cy.get("#search-box");
     getSearchAutoCompletionBox = () => cy.get('div#search-box-completion li');
+    getUserDropdownlink =()=> cy.get('.login > .model-link > .jenkins-menu-dropdown-chevron') 
+    getDropdownConfigureItem =()=> cy.get ('.jenkins-dropdown > [href*="/configure"]') 
 
     typeSearchTerm(term) {
         this.getSearchField().type(term);
@@ -26,6 +28,13 @@ class Header {
         this.getSearchField().type(`${input}{enter}`);
         return new SearchResultsPage();
     };
+    clickUserDropdownLink() {
+        this.getUserDropdownlink().click({ force: true }) 
+        return this;
+    }
+    clickUserConfigureItem() {
+        this.getDropdownConfigureItem().click({ force: true })
+        return this
+    }
 };
-
 export default Header;
