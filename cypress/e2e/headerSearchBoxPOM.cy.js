85bf78d5c5d38d58eaf038ce9a1b25a9c06a5d1a
Svitlana Andrushenko <wwwli4e@gmail.com>
2024-11-27 21:43:27 +0200

    RF: Add methods in FolderPage, PipelinePage, use new methods in test.

--- cypress/e2e/headerSearchBoxPOM.cy.js
+++ cypress/e2e/headerSearchBoxPOM.cy.js
@@ -5,119 +5,125 @@ import SearchResuls from "../pageObjects/SearchResultsPage";
 import DashboardPage from "../pageObjects/DashboardPage";
 import UserConfigurePage from "../pageObjects/UserConfigurePage";
 import FreestyleProjectPage from "../pageObjects/FreestyleProjectPage";
-
 import headerData from "../fixtures/headerData.json";
 import searchResultsData from "../fixtures/searchResultsData.json";
 import messages from "../fixtures/messages.json";
 import newJobPageData from "../fixtures/newJobPageData.json";
-import configurePageData from "../fixtures/configurePageData.json"
+import configurePageData from "../fixtures/configurePageData.json";
 import NewJobPage from "../pageObjects/NewJobPage";
 import FolderPage from "../pageObjects/FolderPage";
+import PipelinePage from "../pageObjects/PipelinePage";
 
 const header = new Header();
-const jobPage = new JobPage();
 const newJobPage = new NewJobPage();
 const dashboardPage = new DashboardPage();
 const searchResults = new SearchResuls();
 const userConfigurePage = new UserConfigurePage();
 const freestyleProjectPage = new FreestyleProjectPage();
 const folderPage = new FolderPage();
+const pipelinePage = new PipelinePage();
 
-describe('US_14.002 | Header > Search Box', () => {
-
+describe("US_14.002 | Header > Search Box", () => {
   it("TC_14.002.05 | User can select suggestion to auto-fill and complete the search", () => {
-    dashboardPage.addNewProject()
-                 .addNewProjectName(newJobPageData.projectName)
-                 .selectFreestyleProject()
-                 .clickOKButton();
-    freestyleProjectPage.typeJobDescription(configurePageData.projectDescription)
-                        .clickSaveButton();
-    header.typeSearchTerm(newJobPageData.projectName)
-          .clickFirstOptionFromACBox()
-          .searchTerm();
+    dashboardPage
+      .addNewProject()
+      .addNewProjectName(newJobPageData.projectName)
+      .selectFreestyleProject()
+      .clickOKButton();
+    freestyleProjectPage
+      .typeJobDescription(configurePageData.projectDescription)
+      .clickSaveButton();
+    header
+      .typeSearchTerm(newJobPageData.projectName)
+      .clickFirstOptionFromACBox()
+      .searchTerm();
 
-    freestyleProjectPage.getJobHeadline()
-                        .should('have.text', newJobPageData.projectName);
+    freestyleProjectPage
+      .getJobHeadline()
+      .should("have.text", newJobPageData.projectName);
   });
 
-  it('TC_14.002.06 | Multiple matches are displayed on the result page', () => {
-    header.search('conf');
+  it("TC_14.002.06 | Multiple matches are displayed on the result page", () => {
+    header.search("conf");
 
-    searchResults.getConfigItem().should('contain.text', 'config');
-    searchResults.getConfigureItem().should('contain.text', 'configure');
+    searchResults.getConfigItem().should("contain.text", "config");
+    searchResults.getConfigureItem().should("contain.text", "configure");
   });
 
-  it('TC_14.002.07 | Verify the search box provides auto-completion', () => {
-
+  it("TC_14.002.07 | Verify the search box provides auto-completion", () => {
     header.typeSearchTerm(headerData.search.input.matchForCon);
-    
-    header.getSearchAutoCompletionBox()
-          .filter(':visible')
-          .should('have.length', headerData.search.autoCompletionItems.length)
-          .each((item, index) => {
-            cy.wrap(item).should('have.text', headerData.search.autoCompletionItems[index]);
-          });
+
+    header
+      .getSearchAutoCompletionBox()
+      .filter(":visible")
+      .should("have.length", headerData.search.autoCompletionItems.length)
+      .each((item, index) => {
+        cy.wrap(item).should(
+          "have.text",
+          headerData.search.autoCompletionItems[index]
+        );
+      });
   });
 
-  it('TC_14.002.09 | Verify that the selection of an auto-complete suggestion redirects to the relevant page', () => {
+  it("TC_14.002.09 | Verify that the selection of an auto-complete suggestion redirects to the relevant page", () => {
+    header
+      .typeSearchTerm(headerData.search.input.matchForLo)
+      .clickFirstOptionFromACBox()
+      .searchTerm();
 
-    header.typeSearchTerm(headerData.search.input.matchForLo)
-          .clickFirstOptionFromACBox()
-          .searchTerm();
-
-    searchResults.getTitle()
-                 .should('include.text', searchResultsData.title.logRecorders);
-
+    searchResults
+      .getTitle()
+      .should("include.text", searchResultsData.title.logRecorders);
   });
 
   it("TC_14.002.03 | Verify that user can not see suggested results searched with with Upper Case characters with Insensitive mode being on", () => {
-    header.clickUserDropdownLink()
-          .clickUserConfigureItem();
-    userConfigurePage.checkCheckBox()
-                     .clickOnSaveBtn();
+    header.clickUserDropdownLink().clickUserConfigureItem();
+    userConfigurePage.checkCheckBox().clickOnSaveBtn();
     header.typeSearchTerm(headerData.search.input.upperCaseMatchForManage);
 
-    header.getSearchAutoCompletionBox()
-          .should('have.text', headerData.search.searchSuggestions.manage);
-  })
-
-  it('TC_14.002.10 | Verify that the warning message is displayed when no matches are found', () => {
-
-    header.search(headerData.search.input.noMatches);
-
-    searchResults.getNoMatchesErrorMessage()
-                 .should('have.text', messages.search.noMatchesError);
-
+    header
+      .getSearchAutoCompletionBox()
+      .should("have.text", headerData.search.searchSuggestions.manage);
   });
 
-  it('TC_14.002-08 | Case insensitive search', () => {
+  it("TC_14.002.10 | Verify that the warning message is displayed when no matches are found", () => {
+    header.search(headerData.search.input.noMatches);
+
+    searchResults
+      .getNoMatchesErrorMessage()
+      .should("have.text", messages.search.noMatchesError);
+  });
+
+  it("TC_14.002-08 | Case insensitive search", () => {
     header.clickUserDropdownLink();
     header.clickUserConfigureItem();
 
-    userConfigurePage.getInsensitiveSearchLabel().should('contain', 'Insensitive search tool');
-    userConfigurePage.getInsensitiveSearchCheckBox()
-      .should('exist').and('be.checked');
+    userConfigurePage
+      .getInsensitiveSearchLabel()
+      .should("contain", "Insensitive search tool");
+    userConfigurePage
+      .getInsensitiveSearchCheckBox()
+      .should("exist")
+      .and("be.checked");
   });
-  
-  it('TC_14.002.15_A | Verify a User can select a suggestion to auto-fill the search box and complete the search', ()=>{
+
+  it("TC_14.002.15 | Verify suggestions in the search box", () => {
     dashboardPage.clickNewItemMenuLink();
-    newJobPage. addNewProjectName("New Folder TC_14.002.15_A")
-    newJobPage.selectFolder();
-    newJobPage.clickOKButton();
-    userConfigurePage.clickOnSaveBtn()
+    newJobPage
+      .addNewProjectName("New Folder TC_14.002.15_A")
+      .selectFolder()
+      .clickOKButton();
+    userConfigurePage.clickOnSaveBtn();
     folderPage.clickNewItemMenuLink();
-    newJobPage.addNewPrgNameFromFolder("Project TC_14.002.15_A");
-    newJobPage.selectPipelineProject();
-    newJobPage.clickOKButton();
-    userConfigurePage.clickOnSaveBtn()
-    header.getJenkinsLogo();
-    header.typeSearchTerm('Pro');
-    header.clickFirstOptionFromACBox();
-    header.typeSearchTerm('{enter}');
-    // cy.get('.job-index-headline').should('have.text', 'Project TC_14.002.15_A')
-    jobPage.getHeadlineIndex().should('have.text', 'Project TC_14.002.15_A');
-    })
-
-
-
+    newJobPage
+      .addNewPrgNameFromFolder("Project TC_14.002.15_A")
+      .selectPipelineProject()
+      .clickOKButton();
+    pipelinePage.clickOnSaveBtn();
+    header.getJenkinsLogo()
+    header.typeSearchTerm("Pro")
+      .clickFirstOptionFromACBox()
+      .typeSearchTerm("{enter}");
+    freestyleProjectPage.getJobHeadline().should("have.text", "Project TC_14.002.15_A");
+  });
 });
--- cypress/pageObjects/FolderPage.js
+++ cypress/pageObjects/FolderPage.js
@@ -5,11 +5,17 @@ import NewJobPage from "./NewJobPage";
 class FolderPage {
      
     getNewItemLink = () => cy.get('span.task-link-text').contains('New Item');
+    getFolderSaveBtn = () => cy.get('.jenkins-submit-button');
 
     clickNewItemMenuLink() {
         this.getNewItemLink().click({ force: true });
         return new NewJobPage();
       }
+
+      clickOnSaveBtn() {
+        this.getFolderSaveBtn().click()
+        return this
+    }
 
 }
 
