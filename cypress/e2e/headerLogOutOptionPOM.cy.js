c92648b46a966aa3c741f00d602646dd888ff1fc
Anton Kolupaiev <antnikol@gmail.com>
2024-11-27 18:42:45 +0200

    upd/ corrections according to comments

--- cypress/e2e/headerLogOutOptionPOM.cy.js
+++ cypress/e2e/headerLogOutOptionPOM.cy.js
@@ -1,18 +1,20 @@
  /// <reference types="cypress" />
 
- import {sessionIdCookie} from "../fixtures/dashboardPageData.json"
- import DashboardPage from "../pageObjects/DashboardPage"
- import LoginPage from "../pageObjects/LoginPage";
+import DashboardPage from "../pageObjects/DashboardPage"
+import LoginPage from "../pageObjects/LoginPage"
+import {sessionIdCookie} from "../fixtures/dashboardPageData.json"
  
-  describe('US_14.003 | Header > Log out option', () => {
+describe('US_14.003 | Header > Log out option', () => {
  
-   const dashboardPage = new DashboardPage();
-   const loginPage = new LoginPage();
+  const dashboardPage = new DashboardPage()
+  const loginPage = new LoginPage()
  
-   it("TC_14.003.03 | All session-related cookies are cleared", () => {
-     dashboardPage.getSessionCookie(sessionIdCookie).then((sessionCookie) => {
-       dashboardPage.clickLogOutButton();
-       loginPage.checkSessionCookieChanged(sessionCookie, sessionIdCookie);
-     });
-   });
- });
+  it("TC_14.003.03 | All session-related cookies are cleared", () => {
+    dashboardPage.getSessionCookie(sessionIdCookie).then((sessionCookie) => {
+      dashboardPage.clickLogOutButton()
+      loginPage.getSessionCookie(sessionIdCookie).then((updatedSessionCookie) => {
+        expect(sessionCookie).not.to.equal(updatedSessionCookie)
+      });
+    });
+  });
+});
--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -5,11 +5,5 @@ class LoginPage
         return (cookies.find((cookie) => cookie.name.includes(cookieName))).value;
       });
    }
-
-   checkSessionCookieChanged(initialCookieValue, cookieName) {
-      this.getSessionCookie(cookieName).then((updatedCookieValue) => {
-        expect(initialCookieValue).not.to.equal(updatedCookieValue);
-      });
-   }
-}
+} 
 export default LoginPage
