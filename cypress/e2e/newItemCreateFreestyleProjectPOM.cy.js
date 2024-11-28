e550ca4a15a389324cd17d2f3a1052f538e53be2
LubovLepekhina <lubov.lepekhina@gmail.com>
2024-11-28 13:18:10 +0100

    RF_00.001.10

--- cypress/e2e/newItemCreateFreestyleProjectPOM.cy.js
+++ cypress/e2e/newItemCreateFreestyleProjectPOM.cy.js
@@ -5,10 +5,12 @@ import { faker } from '@faker-js/faker';
 import DashboardPage from '../pageObjects/DashboardPage';
 import NewJobPage from '../pageObjects/NewJobPage';
 import FreestyleProjectPage from '../pageObjects/FreestyleProjectPage';
+import Header from '../pageObjects/Header';
 
 const dashboardPage = new DashboardPage();
 const newJobPage = new NewJobPage();
 const freestyleProjectPage = new FreestyleProjectPage();
+const header = new Header();
 
 const folderName = faker.commerce.product();
 
@@ -26,6 +28,21 @@ describe('US_00.001 | New item > Create Freestyle Project', function () {
         freestyleProjectPage.getJobHeadline()
             .should('be.visible')
             .and('have.text', folderName);
+
+    });
+
+
+    it('TC_00.001.10 | Create Freestyle Project using the "New Item" button', function () {
+        
+        dashboardPage.clickNewItemMenuLink();
+        newJobPage.typeNewItemName(folderName)
+            .selectFreestyleProject()
+            .clickOKButton();
+
+        freestyleProjectPage.clickSaveButton();
+        header.clickJenkinsLogo();
+
+        dashboardPage.getProjectName().should('contain', folderName);
 
     });
 
