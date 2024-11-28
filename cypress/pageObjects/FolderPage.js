294bcc1e7f7c0d8cbbefd55d49cd4f25abe3c29b
AbaevMariya <41700081+AbaevMariya@users.noreply.github.com>
2024-11-28 16:01:55 -0500

    RF_01.006.01 | Move project from the Project Page (#465)

--- cypress/pageObjects/FolderPage.js
+++ cypress/pageObjects/FolderPage.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
+import DashboardPage from "./DashboardPage";
 import Header from "./Header";
 
 class FolderPage extends Header {
     getSaveBtn = () => cy.get(".jenkins-submit-button");
     getTitleConfiguration = () => cy.get("#side-panel h1");
     getFolderNameOnMainPanel = () => cy.get("#main-panel h1");
+    getDashboardBreadcrumbsLink = () => cy.get('#breadcrumbs a[href="/"]');
     getNewItemMenuOption = () => cy.get('[href $= "/newJob"]');
 
     clickSaveBtn () {
@@ -18,12 +20,16 @@ class FolderPage extends Header {
         return this;
     }
 
+    clickDashboardBreadcrumbsLink () {
+        this.getDashboardBreadcrumbsLink().click();
+        return new DashboardPage();
+    }
+
     clickNewItemMenuOption () {
         this.getNewItemMenuOption().click();
         return this;
     }
  
-
 };
 
 export default FolderPage;
