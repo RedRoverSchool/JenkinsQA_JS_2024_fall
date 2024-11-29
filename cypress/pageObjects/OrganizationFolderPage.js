da83b7f3235b399ca7f4c497bdf5d102cd960a4b
Alzhan Braliyev <alzhan.braliyev@gmail.com>
2024-11-29 00:25:20 -0500

    TC_06.005.02 | Delete Organization Folder from breadcrumbs dropdown menu (#472)

--- cypress/pageObjects/OrganizationFolderPage.js
+++ cypress/pageObjects/OrganizationFolderPage.js
@@ -6,8 +6,10 @@ class OrganizationFolderPage {
     getOKButton = () => cy.get('button').contains('Yes');
     getSideMenuDeleteLink = () => cy.get('[class="task "]').contains("Delete Organization Folder");
     getJobHeadline = () => cy.get('#main-panel h1');
+    getBreadcrumbsFolderName = () => cy.get(':nth-child(3) > .model-link');
+    getBreadcrumbsFolderDropdownMenu = () => cy.get(':nth-child(3) > .model-link > .jenkins-menu-dropdown-chevron');
+    getDropdownMenuDeleteLink = () =>  cy.get('.jenkins-dropdown > [href$="elete"]');
     
-
     clickSaveButton() {
         this.getSaveButton().click();
         return this;
@@ -20,6 +22,21 @@ class OrganizationFolderPage {
 
     clickSideMenuDeleteLink() {
         this.getSideMenuDeleteLink().click();
+        return this;
+    }
+
+    selectBreadcrumbsFolderDropdownMenu() {
+        this.getBreadcrumbsFolderName().trigger('mouseover');
+        return this;
+    }
+
+    clickBreadcrumbsFolderDropdownMenu() {
+        this.getBreadcrumbsFolderDropdownMenu().click({force:true});
+        return this; 
+    }
+    
+    clickDropdownMenuDeleteLink() {
+        this.getDropdownMenuDeleteLink().click();
         return this;
     }
 
