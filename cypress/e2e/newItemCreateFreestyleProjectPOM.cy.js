78a2f1b740abee30da3497a2de46f822bbf692af
Alzhan-B <alzhan.braliyev@gmail.com>
2024-11-29 13:08:10 -0500

    Rolled back other tests to their original format before refactoring

--- cypress/e2e/newItemCreateFreestyleProjectPOM.cy.js
+++ cypress/e2e/newItemCreateFreestyleProjectPOM.cy.js
@@ -20,57 +20,57 @@ const folderName = faker.commerce.product();
 describe("US_00.001 | New item > Create Freestyle Project", function () {
   let project = genData.newProject();
 
-  it("TC_00.001.19 | New freestyle project is created if user enter projects name, choose project type and save it", () => {
+  
+  it('TC_00.001.19 | New freestyle project is created if user enter projects name, choose project type and save it', () => {
+
     dashboardPage.clickNewItemMenuLink();
-    newJobPage
-      .typeNewItemName(folderName)
-      .selectFreestyleProject()
-      .clickOKButton();
+    newJobPage.typeNewItemName(folderName)
+        .selectFreestyleProject()
+        .clickOKButton();
     freestyleProjectPage.clickSaveButton();
 
-    cy.url().should("include", folderName);
-    freestyleProjectPage
-      .getJobHeadline()
-      .should("be.visible")
-      .and("have.text", folderName);
-  });
+    cy.url().should('include', folderName);
+    freestyleProjectPage.getJobHeadline()
+        .should('be.visible')
+        .and('have.text', folderName);
+
+});
+
 
   it('TC_00.001.10 | Create Freestyle Project using the "New Item" button', () => {
+
     dashboardPage.clickNewItemMenuLink();
-    newJobPage
-      .typeNewItemName(folderName)
-      .selectFreestyleProject()
-      .clickOKButton();
+    newJobPage.typeNewItemName(folderName)
+        .selectFreestyleProject()
+        .clickOKButton();
 
     freestyleProjectPage.clickSaveButton();
     header.clickJenkinsLogo();
 
-    dashboardPage.getProjectName().should("contain", folderName);
-  });
+    dashboardPage.getProjectName().should('contain', folderName);
 
-  it("TC_00.001.13 | Verify that duplicate names are not allowed during project creation", () => {
+});
+
+it('TC_00.001.13 | Verify that duplicate names are not allowed during project creation', () => {
+
     dashboardPage.clickNewItemMenuLink();
-    newJobPage
-      .typeNewItemName(folderName)
-      .selectFreestyleProject()
-      .clickOKButton();
+    newJobPage.typeNewItemName(folderName)
+        .selectFreestyleProject()
+        .clickOKButton();
     freestyleProjectPage.clickSaveButton();
     header.clickDashboardBtn();
 
     dashboardPage.clickNewItemMenuLink();
 
-    newJobPage
-      .typeNewItemName(folderName)
-      .getItemNameInvalidErrorMessage()
-      .should(
-        "contain.text",
-        `${newItem.duplicateNotAllowedMessage} ‘${folderName}’`
-      );
+    newJobPage.typeNewItemName(folderName)
+        .getItemNameInvalidErrorMessage()
+            .should('contain.text', `${newItem.duplicateNotAllowedMessage} ‘${folderName}’`);
 
-    newJobPage.getOKButton().should("be.disabled");
-  });
+    newJobPage.getOKButton().should('be.disabled');
 
-  it("TC_00.001.11 | Create Freestyle Project by clicking on 'Create a Job' link", function () {
+});
+
+  it("TC_00.001.11 | Create Freestyle Project by clicking on Create a Job", function () {
     dashboardPage.clickCreateJobLink();
     newJobPage
       .typeNewItemName(project.name)
