32962035d40d4ec43985d5d48652ccc63931003f
Belkacole <colesnicova777@gmail.com>
2024-11-29 14:43:47 -0500

    new project is created

--- cypress/e2e/newItemCreateFreestyleProjectPOM.cy.js
+++ cypress/e2e/newItemCreateFreestyleProjectPOM.cy.js
@@ -1,13 +1,13 @@
 /// <reference types="cypress" />
 
-import { faker } from '@faker-js/faker';
+import { faker } from "@faker-js/faker";
 
-import DashboardPage from '../pageObjects/DashboardPage';
-import NewJobPage from '../pageObjects/NewJobPage';
-import FreestyleProjectPage from '../pageObjects/FreestyleProjectPage';
-import Header from '../pageObjects/Header';
+import DashboardPage from "../pageObjects/DashboardPage";
+import NewJobPage from "../pageObjects/NewJobPage";
+import FreestyleProjectPage from "../pageObjects/FreestyleProjectPage";
+import Header from "../pageObjects/Header";
 
-import { newItem } from '../fixtures/messages.json'
+import { newItem } from "../fixtures/messages.json";
 
 const dashboardPage = new DashboardPage();
 const newJobPage = new NewJobPage();
@@ -16,55 +16,66 @@ const header = new Header();
 
 const folderName = faker.commerce.product();
 
-describe('US_00.001 | New item > Create Freestyle Project', function () {
+describe("US_00.001 | New item > Create Freestyle Project", function () {
+  it("TC_00.001.19 | New freestyle project is created if user enter projects name, choose project type and save it", () => {
+    dashboardPage.clickNewItemMenuLink();
+    newJobPage
+      .typeNewItemName(folderName)
+      .selectFreestyleProject()
+      .clickOKButton();
+    freestyleProjectPage.clickSaveButton();
 
-    it('TC_00.001.19 | New freestyle project is created if user enter projects name, choose project type and save it', () => {
+    cy.url().should("include", folderName);
+    freestyleProjectPage
+      .getJobHeadline()
+      .should("be.visible")
+      .and("have.text", folderName);
+  });
 
-        dashboardPage.clickNewItemMenuLink();
-        newJobPage.typeNewItemName(folderName)
-            .selectFreestyleProject()
-            .clickOKButton();
-        freestyleProjectPage.clickSaveButton();
+  it('TC_00.001.10 | Create Freestyle Project using the "New Item" button', () => {
+    dashboardPage.clickNewItemMenuLink();
+    newJobPage
+      .typeNewItemName(folderName)
+      .selectFreestyleProject()
+      .clickOKButton();
 
-        cy.url().should('include', folderName);
-        freestyleProjectPage.getJobHeadline()
-            .should('be.visible')
-            .and('have.text', folderName);
+    freestyleProjectPage.clickSaveButton();
+    header.clickJenkinsLogo();
 
-    });
+    dashboardPage.getProjectName().should("contain", folderName);
+  });
 
+  it("TC_00.001.13 | Verify that duplicate names are not allowed during project creation", () => {
+    dashboardPage.clickNewItemMenuLink();
+    newJobPage
+      .typeNewItemName(folderName)
+      .selectFreestyleProject()
+      .clickOKButton();
+    freestyleProjectPage.clickSaveButton();
+    header.clickDashboardBtn();
 
-    it('TC_00.001.10 | Create Freestyle Project using the "New Item" button', () => {
-        
-        dashboardPage.clickNewItemMenuLink();
-        newJobPage.typeNewItemName(folderName)
-            .selectFreestyleProject()
-            .clickOKButton();
+    dashboardPage.clickNewItemMenuLink();
 
-        freestyleProjectPage.clickSaveButton();
-        header.clickJenkinsLogo();
+    newJobPage
+      .typeNewItemName(folderName)
+      .getItemNameInvalidErrorMessage()
+      .should(
+        "contain.text",
+        `${newItem.duplicateNotAllowedMessage} ‘${folderName}’`
+      );
 
-        dashboardPage.getProjectName().should('contain', folderName);
+    newJobPage.getOKButton().should("be.disabled");
+  });
 
-    });
+  it.only("TC_00.001.21 | New freestyle project is created if user enter projects' name, choose project type and save it.", () => {
+    dashboardPage.clickNewItemMenuLink();
+    newJobPage
+      .typeNewItemName(folderName)
+      .selectFreestyleProject()
+      .clickOKButton();
+    freestyleProjectPage.clickSaveButton();
 
-    it('TC_00.001.13 | Verify that duplicate names are not allowed during project creation', () => {
-        
-        dashboardPage.clickNewItemMenuLink();
-        newJobPage.typeNewItemName(folderName)
-            .selectFreestyleProject()
-            .clickOKButton();
-        freestyleProjectPage.clickSaveButton();
-        header.clickDashboardBtn();
-   
-        dashboardPage.clickNewItemMenuLink();
-
-        newJobPage.typeNewItemName(folderName)
-            .getItemNameInvalidErrorMessage()
-                .should('contain.text', `${newItem.duplicateNotAllowedMessage} ‘${folderName}’`);
-        
-        newJobPage.getOKButton().should('be.disabled');
-
-    });
-
+    header.clickJenkinsLogo();
+    dashboardPage.getProjectName().should("contain", folderName);
+  });
 });
