7013a1d5443d1651ffec98160d42f604d4ce55dd
Afa Abbasli <aabbasli2020@gmail.com>
2024-11-29 15:47:05 -0700

    TC_04.004-03  Folder > Add or Edit Description of a Folder|Enter&Save description in a special field'

--- cypress/e2e/folderAddOrEditDescriptionOfAFolder.cy.js
+++ cypress/e2e/folderAddOrEditDescriptionOfAFolder.cy.js
@@ -32,4 +32,21 @@ describe('US_04.004 | Folder > Add or Edit Description of a Folder', () => {
             cy.get('button.jenkins-submit-button').click;
             cy.get('div#description').should('be.visible', 'Updated description of the folder');
     });
+
+    it('TC_04.004-03  Folder > Add or Edit Description of a Folder|Enter&Save description in a special field', () => {
+        //Preconditions
+        const folder1Name = 'Folder1';
+            cy.get('span').contains('New Item').click();
+            cy.get('input[name="name"]').clear();
+            cy.get('input[name="name"]').type(folder1Name);
+            cy.get('#j-add-item-type-nested-projects .j-item-options li[class*="folder_Folder"]').click();
+            cy.get('#ok-button').click();
+
+            cy.get('textarea.jenkins-input').type('This is the test description!');
+            cy.get('a.textarea-show-preview').click();
+            cy.get('div.textarea-preview').should('be.visible',"This is the test description!");
+            cy.get('a.textarea-hide-preview').click();
+            cy.get('.jenkins-submit-button').click();
+            cy.get('div#view-message').should('have.text',"This is the test description!");
+    });
 })
