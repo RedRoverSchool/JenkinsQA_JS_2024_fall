77fbdfb5fa621c3c003fbd32e007fad7a7c7dac9
Alzhan-B <alzhan.braliyev@gmail.com>
2024-11-28 19:24:41 -0500

    TC_06.005.01 | Delete Organization Folder from a sidebar menu

--- cypress/pageObjects/OrganizationFolderPage.js
+++ cypress/pageObjects/OrganizationFolderPage.js
@@ -2,6 +2,27 @@
 
 class OrganizationFolderPage {
 
+    getSaveButton = () => cy.get('button[name="Submit"]');
+    getOKButton = () => cy.get('button').contains('Yes');
+    getSideMenuDeleteLink = () => cy.get('[class="task "]').contains("Delete Organization Folder");
+    getJobHeadline = () => cy.get('#main-panel h1');
+    
+
+    clickSaveButton() {
+        this.getSaveButton().click();
+        return this;
+    }
+
+    clickOKButton() {
+        this.getOKButton().click();
+        return this;
+    }
+
+    clickSideMenuDeleteLink() {
+        this.getSideMenuDeleteLink().click();
+        return this;
+    }
+
 }
 
 export default OrganizationFolderPage;
