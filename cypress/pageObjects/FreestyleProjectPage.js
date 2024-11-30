2030eebeedb74ce7891d2aeaaa5be7aea56ca52f
Ion Negrescu <Negrescu29@users.noreply.github.com>
2024-11-30 20:47:04 +0100

    RF_01.002.10 | Receive an Error when the new name is invalid

--- cypress/pageObjects/FreestyleProjectPage.js
+++ cypress/pageObjects/FreestyleProjectPage.js
@@ -22,7 +22,8 @@ class FreestyleProjectPage {
     getNewNameField = () => cy.get('[name="newName"]');
     getRenameButtonSubmit = () => cy.get('button.jenkins-submit-button');
     getBreadcrumbBar = () => cy.get('#breadcrumbBar');
-
+    getHeaderOnRename = () => cy.get("div h1")
+    getErrorMessageParagraph = () => cy.get('p')
 
     clickSaveButton() {
         this.getSaveButton().click();
@@ -102,6 +103,25 @@ class FreestyleProjectPage {
         this.getRenameButtonSubmit().click();
         return this;
     }
-}
 
+    validateSpecialCharacters() {
+        const specialChars = `!@#$%^*|[]\\:;\?/`.split("");
+
+        specialChars.forEach((char) => {
+            // Clear, type the new name, and click Save
+            this.getNewNameField().clear().type(`Rename${char}Folder`);
+            this.getSaveButton().click();
+
+            // Assertions for error messages
+            this.getHeaderOnRename().should("have.text", "Error");
+            this.getErrorMessageParagraph().should(
+                "have.text",
+                `‘${char}’ is an unsafe character`
+            );
+
+            // Navigate back to retry the next character
+            cy.go("back");
+        });
+    }
+}
 export default FreestyleProjectPage;
