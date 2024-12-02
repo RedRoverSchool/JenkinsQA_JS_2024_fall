74ee9d223c00d6b62b4d6b5e3463d350d1acd01a
Jul-Sol-RS <166362972+Jul-Sol-RS@users.noreply.github.com>
2024-12-02 22:46:41 +0500

    RF_14.003.05 | Header > Log out option | Verify user log out the current session on click "Log out" link. (#581)

--- cypress/pageObjects/LoginPage.js
+++ cypress/pageObjects/LoginPage.js
@@ -1,11 +1,12 @@
 class LoginPage
 {
+   getHeader = () => cy.get('h1').contains('Sign in to Jenkins');
+   getSignInButton = () => cy.get('button.jenkins-button--primary');   
+
    getSessionCookie(cookieName) {
       return cy.getCookies().then((cookies) => {
         return (cookies.find((cookie) => cookie.name.includes(cookieName))).value;
       });
-   }
-   getHeader = () => cy.get('h1').contains('Sign in to Jenkins');
-   getSignInButton = () => cy.get('button.jenkins-button--primary');   
+   }   
 }
 export default LoginPage
