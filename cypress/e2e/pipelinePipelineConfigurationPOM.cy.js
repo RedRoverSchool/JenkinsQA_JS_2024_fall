e5508198c6100ff536d0af972bdb7e2eca08784d
Anton Kolupaiev <antnikol@gmail.com>
2024-12-03 19:07:10 +0200

    TC_02.004.03 | Pipeline > Pipeline Configuration | Modify the description field for the pipeline

---
+++ cypress/e2e/pipelinePipelineConfigurationPOM.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress"/>
+
+import { faker } from "@faker-js/faker";
+import DashboardPage from "../pageObjects/DashboardPage";
+import NewJobPage from "../pageObjects/NewJobPage";
+import PipelinePage from "../pageObjects/PipelinePage";
+
+
+const dashboardPage = new DashboardPage();
+const newJobPage = new NewJobPage();
+const pipelinePage = new PipelinePage();
+
+
+describe('US_02.004 | Pipeline > Pipeline Configuration', () => {
+
+  const randomItemName = faker.commerce.productName();
+  const pipelineDescription = faker.lorem.paragraph();
+  const newPipelineDescription = faker.lorem.paragraph()
+
+    it('TC_02.004.03 | Modify the description field for the pipeline', () => {
+      dashboardPage.clickNewItemMenuLink()
+      newJobPage.typeNewItemName(randomItemName)
+                .selectPipelineProject()
+                .clickOKButton()
+      pipelinePage.typePipelineDescription(pipelineDescription)
+                  .clickOnSaveBtn()
+                  .clickConfigurePipelineMenuButton()
+                  .clearPipelineDescriptionField()
+                  .typePipelineDescription(newPipelineDescription)
+                  .clickOnSaveBtn()
+                  .getPipelineJobDescription()
+                  .should('contain.text', newPipelineDescription)
+    })
+
+})
--- cypress/pageObjects/PipelinePage.js
+++ cypress/pageObjects/PipelinePage.js
@@ -3,11 +3,30 @@
 class PipelinePage {
 
     getPipelineSaveBtn = () => cy.get('.jenkins-submit-button');
+    getPipelineDescriptionField = () => cy.get('textarea[name="description"]')
+    getConfigurePipelineMenuButton = () => cy.get('a[href$="configure"]')
+    getPipelineJobDescription = () => cy.get('#description')
 
     clickOnSaveBtn() {
         this.getPipelineSaveBtn().click()
         return this
     }
+
+    typePipelineDescription(description) {
+        this.getPipelineDescriptionField().type(description)
+        return this
+    }
+
+    clickConfigurePipelineMenuButton() {
+        this.getConfigurePipelineMenuButton().click()
+        return this
+    }
+
+    clearPipelineDescriptionField() {
+        this.getPipelineDescriptionField().clear()
+        return this
+    }
+
 
 }
 
