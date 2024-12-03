dbf6849ddfd88f7bf40640659024dd542cfea0b7
Maria Mednikova <mariamednikova9311@gmail.com>
2024-12-02 19:22:03 -0500

    deleted extra pages

--- cypress/e2e/dashboardCreateViewPOM.cy.js
+++ cypress/e2e/dashboardCreateViewPOM.cy.js
@@ -5,9 +5,6 @@ import NewJobPage from "../pageObjects/NewJobPage";
 import FreestyleProjectPage from "../pageObjects/FreestyleProjectPage";
 import Header from "../pageObjects/Header";
 import MyViewsPage from "../pageObjects/MyViewsPage";
-import NewViewPage from "../pageObjects/NewViewPage";
-import NewViewConfigurePage from "../pageObjects/NewViewConfigurePage";
-import UsersCurrentViewPage from "../pageObjects/UsersCurrentViewPage";
 
 import genData from "../fixtures/genData";
 
@@ -15,10 +12,7 @@ const dashboardPage = new DashboardPage();
 const newJobPage = new NewJobPage();
 const freestyleProjectPage = new FreestyleProjectPage();
 const header = new Header();
-const newViewPage = new NewViewPage();
 const myViewsPage = new MyViewsPage();
-const newViewConfigurePage = new NewViewConfigurePage();
-const usersCurrentViewPage = new UsersCurrentViewPage();
 
 describe("US_16.002 | Dashboard > Create View", () => {
   let project = genData.newProject();
@@ -40,25 +34,24 @@ describe("US_16.002 | Dashboard > Create View", () => {
     header.clickJenkinsLogo();
   });
 
-  it("TC_16.002.01 Verify user is able to create global view from the Dashboard page", () => {
+  it("TC_16.002.01 Create global view from the Dashboard page", () => {
     dashboardPage.clickMyViewsLink();
-    myViewsPage.clickAddNewViewLink();
-
-    newViewPage
-      .typeViewName(view.name)
-      .clickIncludeGlobalViewButton()
-      .clickCreateButton();
-    newViewConfigurePage.clickOkButton();
+    myViewsPage.clickAddNewViewLink()
+               .typeViewName(view.name)
+               .clickIncludeGlobalViewButton()
+               .clickCreateButton()
+               .clickOkButton();
 
     cy.url().then((url) => {
       const normalizedUrl = url.replace("%20", " ");
       expect(normalizedUrl).to.contain(view.name);
     });
-    usersCurrentViewPage
-      .getCurrentViewBreadcrumbsItem()
-      .should("have.text", view.name);
-    usersCurrentViewPage.clickMyViewsBreadcrumbsItem();
+    myViewsPage.getCurrentViewBreadcrumbsItem()
+              .should("have.text", view.name);
+    myViewsPage.clickMyViewsBreadcrumbsItem()
+              .getViewTab(view.name)
+              .should("be.visible");
 
-    myViewsPage.getViewTab(view.name).should("be.visible");
   });
+
 });
