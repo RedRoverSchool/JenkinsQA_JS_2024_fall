578493cf2c7483a7353b7a77223401290778d890
Olha Dymaretska <olhadymaretska@gmail.com>
2024-12-05 18:00:02 -0500

    TC_00.002.16 | New Item > Create Pipeline Project #631

--- cypress/e2e/oldTests/createPipelineProject.cy.js
+++ cypress/e2e/oldTests/createPipelineProject.cy.js
@@ -16,4 +16,14 @@ describe.skip('US_00.002 | New Item > Create Pipeline Project #14', () => {
         cy.get(`button[data-href*="/job/${projectname}"]`).click();
         cy.get(`button[href="/job/${projectname}/doDelete"]`).click();
         cy.get('button[data-id="ok"]').click()
-    })})
+    })
+  it('TC_00.002.16 | New Item > Create  a new Pipeline Project', () => {
+        cy.contains('span[class="task-link-text"]', 'New Item').click({force:true})
+        cy.get('.org_jenkinsci_plugins_workflow_job_WorkflowJob').click()
+        cy.get('.jenkins-input').type('Olga Project')
+        cy.get('#ok-button').click()
+        cy.get('button[name="Submit"]').click()
+        cy.get('li.jenkins-breadcrumbs__list-item').click()
+        cy.get('.jenkins-table__link > span').should('be.visible').and('contain', 'Olga Project')
+    })
+})
