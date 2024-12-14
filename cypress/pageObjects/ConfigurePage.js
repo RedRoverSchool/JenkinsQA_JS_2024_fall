c053615a6fb02f3a34b2a40c5b37ce67cfe87c4c
yanaoreshkevich <Yaninaoreshkevich@gmail.com>
2024-12-14 22:17:19 +0300

    deleted method typeDisplayName

--- cypress/pageObjects/ConfigurePage.js
+++ cypress/pageObjects/ConfigurePage.js
@@ -1,20 +1,13 @@
 /// <reference types="cypress" />
 import ManageJenkinsPage from "../pageObjects/ManageJenkinsPage";
-import BasePage from "./basePage";
 
-class ConfigurePage extends BasePage {
+class ConfigurePage {
    
     getBreadcrumbsManageJenkins = () => cy.get('[href="/manage/"]');
-    getDisplayNameField = () => cy.get('input[class="jenkins-input validated  "]');
-
+    
     clickBreadcrumbsManageJenkins() {
         this.getBreadcrumbsManageJenkins().click();
         return new ManageJenkinsPage();
-    }
-
-    typeDisplayName = (displayName) => {
-        this.getDisplayNameField().type(displayName);
-        return this;
     }
 };
 export default ConfigurePage;
