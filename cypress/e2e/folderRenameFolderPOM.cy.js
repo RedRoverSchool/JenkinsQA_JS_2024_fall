2c166b9263b39b7e2bba1ffc45551aa763a0281b
yanaoreshkevich <Yaninaoreshkevich@gmail.com>
2024-12-14 16:36:20 +0300

    TC_04.001.11 | Folder > Rename Folder | Rename a folder on the folder page in the Configure section

--- cypress/e2e/folderRenameFolderPOM.cy.js
+++ cypress/e2e/folderRenameFolderPOM.cy.js
@@ -4,12 +4,14 @@ import DashboardPage from "../pageObjects/DashboardPage";
 import NewJobPage from "../pageObjects/NewJobPage";
 import FolderPage from "../pageObjects/FolderPage";
 import Header from "../pageObjects/Header";
+import OrganizationFolderPage from "../pageObjects/OrganizationFolderPage";
 import genData from "../fixtures/genData";
 
 const dashboardPage = new DashboardPage();
 const newJobPage = new NewJobPage();
 const folderPage = new FolderPage();
 const header = new Header();
+const organizationFolderPage = new OrganizationFolderPage();
 const folderName = genData.newProject();
 const newFolderName = genData.newProject();
 
@@ -79,5 +81,18 @@ describe('US_04.001 | Folder > Rename Folder', () => {
 
         folderPage.getFolderNameOnMainPanel()
             .should('include.text', `${newFolderName.name}`);
+    });
+
+    it('TC_04.001.11 | Rename a folder on the folder page in the Configure section', () => {
+        
+        dashboardPage.clickItemName(folderName.name);
+        folderPage.getFolderNameOnMainPanel().should('include.text', folderName.name);
+        folderPage.clickConfigureLMenuOption();
+        organizationFolderPage.typeDisplayName(newFolderName.name)
+            .clickSaveButton();
+
+        folderPage.getFolderNameOnMainPanel().should('include.text', newFolderName.name);
+        folderPage.clickJenkinsLogo();
+        dashboardPage.getItemName().should('contain', newFolderName.name).and('be.visible');
     });
 });
